8443a725f6b3fcc276878059a2566bec0fe3d9e5
justJstn <mrcjstnmanzano@gmail.com>
2025-04-30 09:51:32 +0800

    123

--- database/migrations/2025_04_30_014802_create_tbl_employee_table.php
+++ database/migrations/2025_04_30_014802_create_tbl_employee_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEmployeesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -34,6 +34,6 @@ class CreateEmployeesTable extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('tbl_employee');
     }
 };
