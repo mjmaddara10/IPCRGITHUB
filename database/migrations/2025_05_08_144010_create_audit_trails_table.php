21c13c73a58bf42bcb66e3bbb8e18be452bb7746
justJstn <mrcjstnmanzano@gmail.com>
2025-05-08 14:43:15 +0800

    123

---
+++ database/migrations/2025_05_08_144010_create_audit_trails_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('audit_trails', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('tbl_employee')->onDelete('cascade');
+
+            $table->string('full_name');
+            $table->string('role');
+            $table->string('action');
+            $table->string('action_from')->nullable();
+            $table->string('action_to')->nullable();
+            $table->string('program_name')->nullable();
+            $table->unsignedBigInteger('record_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('audit_trails');
+    }
+};
